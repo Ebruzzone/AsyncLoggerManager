c6417cef55e904d992713ff7b60fec6faedbafd2
ElBr <bruzzone.elia@gmail.com>
2021-01-06 18:43:59 +0100

    Default properties - Iterators - Other refinings

--- src/main/java/AsyncLogger/AsyncLoggerManager.java
+++ src/main/java/AsyncLogger/AsyncLoggerManager.java
@@ -2,40 +2,53 @@ package AsyncLogger;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
+import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
+@SuppressWarnings("unused")
 public class AsyncLoggerManager {
-
-	private static org.apache.logging.log4j.core.async.AsyncLogger logger;
-
+	
+	private final org.apache.logging.log4j.core.async.AsyncLogger logger;
+	
 	private final ConcurrentLinkedQueue<String> logsTrace;
 	private final ConcurrentLinkedQueue<String> logsDebug;
 	private final ConcurrentLinkedQueue<String> logsInfo;
 	private final ConcurrentLinkedQueue<String> logsWarn;
 	private final ConcurrentLinkedQueue<String> logsError;
 	private final ConcurrentLinkedQueue<String> logsFatal;
-
+	
 	private final AtomicInteger sevMin;
-
-	private AtomicLong wait;
 	private final long waitMax;
-
+	private long wait;
+	
 	private final AtomicBoolean alive = new AtomicBoolean(true);
-
+	
 	public enum Severity {
 		TRACE, DEBUG, INFO, WARN, ERROR, FATAL
 	}
-
-	public AsyncLoggerManager(Severity sevMin, long waitMax) {
-
+	
+	public AsyncLoggerManager() {
+		this(Severity.INFO, 498);
+	}
+	
+	public AsyncLoggerManager(@NotNull Severity sevMin) {
+		this(sevMin, 498);
+	}
+	
+	public AsyncLoggerManager(@NotNull Severity sevMin, long waitMax) {
+		
 		this.waitMax = waitMax * 2 / 3;
-		this.wait = new AtomicLong(waitMax / 5 + 1);
+		this.wait = waitMax / 5 + 1;
 		this.sevMin = new AtomicInteger();
-
+		
 		switch (sevMin) {
 			case DEBUG:
 				this.sevMin.set(1);
@@ -55,119 +68,171 @@ public class AsyncLoggerManager {
 			default:
 				this.sevMin.set(0);
 		}
-
+		
 		logsTrace = new ConcurrentLinkedQueue<>();
 		logsDebug = new ConcurrentLinkedQueue<>();
 		logsInfo = new ConcurrentLinkedQueue<>();
 		logsWarn = new ConcurrentLinkedQueue<>();
 		logsError = new ConcurrentLinkedQueue<>();
 		logsFatal = new ConcurrentLinkedQueue<>();
-
+		
 		new LoggerClassInternal().start();
-
+		
 		System.setProperty("Log4jContextSelector", AsyncLoggerContextSelector.class.getName());
 		logger = (org.apache.logging.log4j.core.async.AsyncLogger) LogManager.getLogger();
 	}
-
+	
+	public enum LoggerType {
+		Console, RandomAccessFile
+	}
+	
+	public static void createLogProperties(@NotNull Severity rootSeverity, String nameLoggerConsole,
+	                                       String patternLoggerConsole, String nameLoggerFile,
+	                                       String patternLoggerFile, String fileName) throws IOException {
+		String t1 = "Console", t2 = "RandomAccessFile";
+		String file = "rootLogger.level = " + rootSeverity.toString() + "\n" + "rootLogger.appenderRefs = " +
+				t1 + "," + t2 + "\nrootLogger.appenderRef." + t1 + ".ref = " + nameLoggerConsole +
+				"\nrootLogger.appenderRef." + t2 + ".ref = " + nameLoggerFile + "\n\nappenders = " + t1 + "," + t2 +
+				"\n\nappender." + t1 + ".type = " + t1 + "\nappender." + t1 + ".name = " + nameLoggerConsole +
+				"\nappender." + t1 + ".layout.type = PatternLayout\nappender." + t1 + ".layout.pattern = " +
+				patternLoggerConsole + "\n\nappender." + t2 + ".type = " + t2 + "\nappender." + t2 + ".name = " +
+				nameLoggerFile + "\nappender." + t2 + ".layout.type = PatternLayout\nappender." + t2 +
+				".layout.pattern = " + patternLoggerFile + "\nappender." + t2 + ".fileName = " + fileName;
+		String path = Objects.requireNonNull(AsyncLoggerManager.class.getClassLoader().getResource("")).getPath();
+		path = path.startsWith("/C:/") ? path.substring(1) : path;
+		Files.write(Paths.get(path + "log4j2.properties"), file.getBytes());
+	}
+	
+	public static void createLogProperties(LoggerType type, String name, String pattern, Severity rootSeverity) throws IOException {
+		createLogProperties(type, name, pattern, rootSeverity, "logs.log");
+	}
+	
+	public static void createLogProperties(@NotNull LoggerType type, String name, String pattern, @NotNull Severity rootSeverity, String fileName) throws IOException {
+		String t = type.toString(), rowFile;
+		if (type == LoggerType.RandomAccessFile) {
+			rowFile = "\nappender." + t + ".fileName = " + fileName;
+		} else {
+			rowFile = "\n";
+		}
+		String file = "rootLogger.level = " + rootSeverity.toString() + "\n" + "rootLogger.appenderRefs = " +
+				t + "\nrootLogger.appenderRef." + t + ".ref = " + name + "\n\nappenders = " + t + "\n\nappender." + t +
+				".type = " + t + "\nappender." + t + ".name = " + name + "\nappender." + t +
+				".layout.type = PatternLayout\nappender." + t + ".layout.pattern = " + pattern + rowFile;
+		String path = Objects.requireNonNull(AsyncLoggerManager.class.getClassLoader().getResource("")).getPath();
+		path = path.startsWith("/C:/") ? path.substring(1) : path;
+		Files.write(Paths.get(path + "log4j2.properties"), file.getBytes());
+	}
+	
+	public void setSeverityMin(@NotNull Severity severityMin) {
+		switch (severityMin) {
+			case DEBUG:
+				this.sevMin.set(1);
+				break;
+			case INFO:
+				this.sevMin.set(2);
+				break;
+			case WARN:
+				this.sevMin.set(3);
+				break;
+			case ERROR:
+				this.sevMin.set(4);
+				break;
+			case FATAL:
+				this.sevMin.set(5);
+				break;
+			default:
+				this.sevMin.set(0);
+		}
+	}
+	
 	public void kill() {
 		alive.set(false);
 	}
-
+	
 	//Method to log with the normal asyncLogger
 	public void logTrace(String s) {
 		if (sevMin.get() == 0) {
-			logsTrace.add(s);
+			logsTrace.offer(s);
 		}
 	}
-
+	
 	public void logDebug(String s) {
 		if (sevMin.get() < 2) {
-			logsDebug.add(s);
+			logsDebug.offer(s);
 		}
 	}
-
+	
 	public void logInfo(String s) {
 		if (sevMin.get() < 3) {
-			logsInfo.add(s);
+			logsInfo.offer(s);
 		}
 	}
-
+	
 	public void logWarn(String s) {
 		if (sevMin.get() < 4) {
-			logsWarn.add(s);
+			logsWarn.offer(s);
 		}
 	}
-
+	
 	public void logError(String s) {
 		if (sevMin.get() < 5) {
-			logsError.add(s);
+			logsError.offer(s);
 		}
 	}
-
+	
 	public void logFatal(String s) {
-		logsFatal.add(s);
+		logsFatal.offer(s);
 	}
-
+	
 	private class LoggerClassInternal extends Thread {
-
+		
 		@Override
 		public void run() {
-
+			
 			while (alive.get()) {
-
+				
 				try {
-					Thread.sleep(wait.get());
+					Thread.sleep(wait);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-
-				long sizeTemp = 0;
-
-				int size = logsTrace.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.trace(logsTrace.poll());
+				
+				long i = 0;
+				
+				for (Iterator<String> iter = logsTrace.iterator(); iter.hasNext(); i++) {
+					logger.trace(iter.next());
+					iter.remove();
 				}
-
-				sizeTemp += size;
-				size = logsDebug.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.debug(logsDebug.poll());
+				
+				for (Iterator<String> iter = logsDebug.iterator(); iter.hasNext(); i++) {
+					logger.debug(iter.next());
+					iter.remove();
 				}
-
-				sizeTemp += size;
-				size = logsInfo.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.info(logsInfo.poll());
+				
+				for (Iterator<String> iter = logsInfo.iterator(); iter.hasNext(); i++) {
+					logger.info(iter.next());
+					iter.remove();
 				}
-
-				sizeTemp += size;
-				size = logsWarn.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.warn(logsWarn.poll());
+				
+				for (Iterator<String> iter = logsWarn.iterator(); iter.hasNext(); i++) {
+					logger.warn(iter.next());
+					iter.remove();
 				}
-
-				sizeTemp += size;
-				size = logsError.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.error(logsError.poll());
+				
+				for (Iterator<String> iter = logsError.iterator(); iter.hasNext(); i++) {
+					logger.error(iter.next());
+					iter.remove();
 				}
-
-				sizeTemp += size;
-				size = logsFatal.size();
-
-				for (int i = 0; i < size; i++) {
-					logger.fatal(logsFatal.poll());
+				
+				for (Iterator<String> iter = logsFatal.iterator(); iter.hasNext(); i++) {
+					logger.fatal(iter.next());
+					iter.remove();
 				}
-
-				if (sizeTemp > wait.get() * 10 && wait.get() > 1) {
-					wait.decrementAndGet();
-				} else if (sizeTemp < wait.get() / 2 && wait.get() < waitMax) {
-					wait.incrementAndGet();
+				
+				if (i > (wait << 3) && wait > 1) {
+					wait--;
+				} else if (i < (wait >> 1) && wait < waitMax) {
+					wait++;
 				}
 			}
 		}
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -4,91 +4,103 @@ import AsyncLogger.AsyncLoggerManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
 
+import java.io.IOException;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-
+		
 		System.out.println("Start");
-
+		
+		try {
+			AsyncLoggerManager.createLogProperties(AsyncLoggerManager.LoggerType.Console, "STDOUT",
+					"%d{DEFAULT} %c{1} - %m%n", AsyncLoggerManager.Severity.DEBUG);
+			AsyncLoggerManager.createLogProperties(AsyncLoggerManager.Severity.DEBUG, "Console",
+					"%d{DEFAULT} %c{1} - %m%n", "File",
+					"%m%n", "logs.log");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		System.setProperty("Log4jContextSelector", AsyncLoggerContextSelector.class.getName());
 		org.apache.logging.log4j.core.async.AsyncLogger logger =
 				(org.apache.logging.log4j.core.async.AsyncLogger) LogManager.getLogger();
-
-		long len = 100000L, t = 0, t1 = 0, t2 = 0, t3 = 0, t4, t5, t6, t7;
-
-		AsyncLoggerManager asyncLoggerManager = new AsyncLoggerManager(AsyncLoggerManager.Severity.INFO, 1200L);
-
+		
+		long len = 100000L, t = 0, t1 = 0, t2 = 0, t3 = 0, t4, t5, t6, t7, wait = 1500;
+		
+		AsyncLoggerManager asyncLoggerManager = new AsyncLoggerManager(AsyncLoggerManager.Severity.INFO, 800L);
+		
 		String str = "CiaociaociaociaociaociaociaociaoCiaociaociaociaociaociaociaociao" +
 				"CiaociaociaociaociaociaociaociaoCiaociaociaociaociaociaociaociao" +
 				"CiaociaociaociaociaociaociaociaoCiaociaociaociaociaociaociaociao" +
 				"CiaociaociaociaociaociaociaociaoCiaociaociaociaociaociaociaociao";
-
-		for (int n = 0; n < 30; n++) {
+		
+		for (int n = 0; n < 7; n++) {
 			t4 = System.currentTimeMillis();
-
+			
 			for (int i = 0; i < len; i++) {
-
+				
 				logger.info(str);
 			}
-
+			
 			t4 = System.currentTimeMillis() - t4;
-
+			
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(wait);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
+			
 			t5 = System.currentTimeMillis();
-
+			
 			for (int i = 0; i < len; i++) {
-
+				
 				asyncLoggerManager.logInfo(str);
 			}
-
+			
 			t5 = System.currentTimeMillis() - t5;
-
+			
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(wait);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-
+			
 			t6 = System.currentTimeMillis();
-
+			
 			for (int i = 0; i < len * 20; i++) {
-
+				
 				logger.trace(str);
 			}
-
+			
 			t6 = System.currentTimeMillis() - t6;
-
+			
 			t7 = System.currentTimeMillis();
-
+			
 			for (int i = 0; i < len * 20; i++) {
-
+				
 				asyncLoggerManager.logTrace(str);
 			}
-
+			
 			t7 = System.currentTimeMillis() - t7;
-
+			
 			t += t4;
 			t1 += t5;
 			t2 += t6;
 			t3 += t7;
 		}
-
+		
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(500);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
+		
 		System.out.println("Time logger info: " + t);
 		System.out.println("Time my logger info: " + t1);
 		System.out.println("Time logger trace: " + t2);
 		System.out.println("Time my logger trace: " + t3);
-
+		
 		asyncLoggerManager.kill();
 	}
 }
